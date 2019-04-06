8050dfd81ef6e706ed7855f32d443b72d7b7e4f4
helga-hai <helga-hai@ukr.net>
2019-04-06 23:46:45 +0300

    12_ES6_DOM_squere_random_EDIT

--- ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
+++ ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
@@ -9,67 +9,7 @@
 
 window.onload = function() {
 
-/************************
-		let params = [
-				document.documentElement.clientHeight, 
-				document.documentElement.clientWidth,
-				];
 
-		var getStyleWidth = function(name){
-			var res = Math.floor(Math.random() * params[1] + parseInt(name.style.width));
-			return res
-		}
-		var getStyleHeight = function(name){
-			return Math.floor(Math.random() * params[0] + parseInt(name.style.height));
-		}
-
-getStyle()
-
-		let divs = document.querySelectorAll('div');
-		for(let i=0; i<divs.Length; i++){
-			divs[i].style.top=params[3]
-			divs[i].style.left=params[3]
-		}
-var res = getStyleWidth
-res(red)	
-console.log(res)
-************************/
-
-/*const PARAMS = [document.documentElement.clientHeight,
-				document.documentElement.clientWidth];
-
-function getSquere(){
-	this.height = parseInt(this.style.height);
-	this.with = parseInt(this.style.width);
-
-	this.data = [parseInt(this.style.height), parseInt(this.style.width)]
-	this.randomPosition = function(){
-		let i = 0,top,left;
-		return function(){
-			var pos = Math.floor(Math.random() * PARAMS[i] + this.data[i]);
-			(i == 0) ? top = pos : left = pos
-			i++
-		}
-	}
-	var frequency =this.randomPosition
-}*/
-
-
-
-/*var blocksParams = {
- 	'red':[100,80],
- 	'green':[90,70],
- 	'blue':[78,50]
-}*/
-/*var bloksParams = new Array();
-for(var i=0; i<divs.length; i++){
-	//for(var key in blocksParams){
-		//blocksParams[key][i] = 
-		bloksParams[i] = parseInt(divs[i].style.height)
-		//blocksParams[key][++i] = 
-		bloksParams[++i] = parseInt(divs[i].style.width)
-	//}
-}*/
 const PARAMS = [document.documentElement.clientHeight,
 				document.documentElement.clientWidth];
 
