b76ea2e818f82151b4c1013cca07939e149e99d6
helga-hai <helga-hai@ukr.net>
2019-04-06 23:23:25 +0300

    12_ES6_DOM_squere_random_EDIT

--- ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
+++ ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
@@ -9,48 +9,94 @@
 
 window.onload = function() {
 
-	let red = document.querySelector('.red');
-	let green = document.querySelector('.green');
-	let blue = document.querySelector('.blue');
+/************************
+		let params = [
+				document.documentElement.clientHeight, 
+				document.documentElement.clientWidth,
+				];
 
-	let toWidth = document.documentElement.clientWidth;
-	let toHeight = document.documentElement.clientHeight;
-
-console.log(toWidth,toHeight);
-
-
-
-	
-//-------------------------------------------------------------
-	var t, l;
-
-	function changePosition(name){
-		t = Math.floor(Math.random() * toHeight - name.style.height);
-		l = Math.floor(Math.random() * toWidth - name.style.width);
-		var pos = {top:t, left:l};
-		//return function(pos){
-			//if(pos==t) name.style.top = t + 'px';
-		//name.style.top = t + 'px';
-		//name.style.left = l + 'px';
-		return function(){
-			for(var key in pos){
-				return name.style[key] = '' + pos[key] + 'px';
-			}
+		var getStyleWidth = function(name){
+			var res = Math.floor(Math.random() * params[1] + parseInt(name.style.width));
+			return res
 		}
-		//}
+		var getStyleHeight = function(name){
+			return Math.floor(Math.random() * params[0] + parseInt(name.style.height));
+		}
+
+getStyle()
+
+		let divs = document.querySelectorAll('div');
+		for(let i=0; i<divs.Length; i++){
+			divs[i].style.top=params[3]
+			divs[i].style.left=params[3]
+		}
+var res = getStyleWidth
+res(red)	
+console.log(res)
+************************/
+
+/*const PARAMS = [document.documentElement.clientHeight,
+				document.documentElement.clientWidth];
+
+function getSquere(){
+	this.height = parseInt(this.style.height);
+	this.with = parseInt(this.style.width);
+
+	this.data = [parseInt(this.style.height), parseInt(this.style.width)]
+	this.randomPosition = function(){
+		let i = 0,top,left;
+		return function(){
+			var pos = Math.floor(Math.random() * PARAMS[i] + this.data[i]);
+			(i == 0) ? top = pos : left = pos
+			i++
+		}
 	}
-	//res = changePosition(red)
-		//console.log(res)
-		console.dir(changePosition(red))
-	setInterval( function(){
+	var frequency =this.randomPosition
+}*/
 
-		document.querySelector('.red').changePosition(red)
-		//red.changePosition();
-		//green.run();
-		//blue.run();
 
-	}, 500)
 
+/*var blocksParams = {
+ 	'red':[100,80],
+ 	'green':[90,70],
+ 	'blue':[78,50]
+}*/
+/*var bloksParams = new Array();
+for(var i=0; i<divs.length; i++){
+	//for(var key in blocksParams){
+		//blocksParams[key][i] = 
+		bloksParams[i] = parseInt(divs[i].style.height)
+		//blocksParams[key][++i] = 
+		bloksParams[++i] = parseInt(divs[i].style.width)
+	//}
+}*/
+const PARAMS = [document.documentElement.clientHeight,
+				document.documentElement.clientWidth];
+
+var divs = document.querySelectorAll('div'); // [div.red, div.green, div,blue]
+
+const SIDES = new Array(divs.length);
+var top, left;
+let position = new Array(divs.length);
+
+for (let i=0; i<SIDES.length; i++) {
+	SIDES[i] = new Array(2);
+	SIDES[i][0] = parseInt(divs[i].style.height);
+	SIDES[i][1] = parseInt(divs[i].style.width);
+	position[i] = new Array(2);
+}
+
+function count(){
+	for (let i=0; i<SIDES.length; i++) {
+		position[i][0] = Math.floor(Math.random() * (PARAMS[0] - SIDES[i][0]));
+		position[i][1] = Math.floor(Math.random() * (PARAMS[1] - SIDES[i][1]));
+		divs[i].style.top = position[i][0] + 'px';
+		divs[i].style.left = position[i][1] + 'px'
+	} // [ [num,num], [num,num], [num,num] ]
+
+}
+
+setInterval(count,1000)
 
 
 }
