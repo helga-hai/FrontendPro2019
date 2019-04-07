cf3faea4807de565044749f09f8f0dde2c648d71
helga-hai <helga-hai@ukr.net>
2019-04-07 08:30:28 +0300

    12_ES6_DOM_squere_random_color

--- ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
+++ ex22_lection_12_ES6_DOM_squere_run/random_blocks_2s.js
@@ -1,10 +1,9 @@
 //Домашнее задание 22
-//Добавлено: 22.03.2019 12:53
 //#2
 //Есть 3 блока различных цветов. Раз в 2 секунды они перемещаются на случайное положение в пределах окна. 
 //---
 //Для выполнения нужно почитать про свойство style в DOM элементе
-
+//Каждое перемещение из задания №2 также меняет цвет блока на случайный. Создать фунцию getRandColor()
 
 
 window.onload = function() {
@@ -13,11 +12,11 @@ window.onload = function() {
 const PARAMS = [document.documentElement.clientHeight,
 				document.documentElement.clientWidth];
 
-var divs = document.querySelectorAll('div'); // [div.red, div.green, div,blue]
+let divs = document.querySelectorAll('div'); // [div.red, div.green, div,blue]
 
 const SIDES = new Array(divs.length);
-var top, left;
 let position = new Array(divs.length);
+let top, left;
 
 for (let i=0; i<SIDES.length; i++) {
 	SIDES[i] = new Array(2);
@@ -31,8 +30,13 @@ function count(){
 		position[i][0] = Math.floor(Math.random() * (PARAMS[0] - SIDES[i][0]));
 		position[i][1] = Math.floor(Math.random() * (PARAMS[1] - SIDES[i][1]));
 		divs[i].style.top = position[i][0] + 'px';
-		divs[i].style.left = position[i][1] + 'px'
-	} // [ [num,num], [num,num], [num,num] ]
+		divs[i].style.left = position[i][1] + 'px';
+		divs[i].style.backgroundColor = 'rgb(' + getRandColor() + ',' + getRandColor() + ',' + getRandColor() + ')';
+	} // [ [top,left], [top,left], [top,left] ]
+
+	function getRandColor(){
+		return Math.floor(Math.random()*255)
+	}
 
 }
 
