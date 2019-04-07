4d0091c75ace129f0d2f264438107ed64f8bc1d2
helga-hai <helga-hai@ukr.net>
2019-04-07 09:19:27 +0300

    lection_12_input_textarea_setinterval_EDIT

--- ex21_lection_12_ES6_DOM/3_input_to_textarea_with_setinterval.js
+++ ex21_lection_12_ES6_DOM/3_input_to_textarea_with_setinterval.js
@@ -1,20 +1,23 @@
 //Домашнее задание 21
-//Добавлено: 22.03.2019 12:52
 //#1
 // 1) Есть 3 `input`. Выводить в `textarea` содержимое всех полей ввода через запятую. Использовать setInterval.
-
-//-----------------------
 
 
 window.onload = function() {
 
-	let first = document.querySelector('input[name=first]');
-	let middle = document.querySelector('input[name=middle]');
-	let last = document.querySelector('input[name=last]');
 	let textarea = document.querySelector('textarea');
+	let inputs = document.querySelectorAll('input');
 
-	setInterval( ()=>{
-	textarea.innerHTML = (first.value +  ',' + middle.value +  ',' + last.value)
-	}, 500)
+
+	function setValue(){
+		let tmp='';
+		for(let i=0; i<inputs.length; i++){
+				tmp += inputs[i].value + ','
+		}
+		tmp = tmp.slice(0, -1);
+		return textarea.innerHTML = tmp
+	}
+
+	setInterval( setValue , 1000)
 
 }
