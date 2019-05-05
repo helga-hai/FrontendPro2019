18214d0066ec86da21fa3e3b99e6c8140d1fc11d
helga-hai <helga-hai@ukr.net>
2019-05-05 18:06:58 +0300

    Match

---
+++ L16-30-match/match.js
@@ -0,0 +1,19 @@
+//Домашнее задание 30
+//Добавлено: 05.04.2019 22:16
+//#2
+//Дано массив строк. В каждой необходимо найти позицию совпадения `match`. Все совпадения сохранить в строку через разделитель.
+//`var match ="hello"`
+//["1234hello", "hellohd;lfmg", "45yu89egrhellowegojih", "dfhghello", ",bnsdklb"]
+//результат: "4, 0, 9, 4"
+
+window.onload = function(){
+	var list = ["1234hello", "hellohd;lfmg", "45yu89egrhellowegojih", "dfhghello", ",bnsdklb"];
+	var match ="hello";
+
+	let res = list
+		.map(item => item.indexOf(match))
+		.filter(item => item >= 0)
+		.join(', ')
+	
+	console.log(res)
+}
