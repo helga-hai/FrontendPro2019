cd855f07d075f32e4f7fafeefa2ac5bbd9d8fb34
helga-hai <helga-hai@ukr.net>
2019-06-09 22:00:18 +0300

    class_ES5_ES6

--- _Node.JS/inherit.js
+++ _Node.JS/inherit.js
@@ -1,37 +1,4 @@
 
-function C1(){
-	//this.renderList = function(){ console.log(this) }
-}
-	C1.prototype.renderList = function(){ console.log(this) }
-
-
-function C2(){}
-	C2.prototype = Object.create(C1.prototype)
-	C2.prototype.fillList = function(from,to){
-		for(var i=0; i<this.list.length; i++){
-			this.list[i] = Math.floor(Math.random()*(to-from+1)+from);
-		}
-		//return this.list
-	}
-	C2.prototype.removeList = function(){ 
-		delete this.list 
-	}
-
-function C3(){
-	this.createList = function(length){
-		this.list =[];
-		this.list.length = length;
-	}
-}
-	C3.prototype = Object.create(C2.prototype)
-
-
-var p = new C3();
-p.createList(10)
-p.fillList(-2,2)
-p.renderList();
-//p.removeList();
-
 /*Написать класс Dog, который наследуется от класcа Animal. Класс Animal имеет метод getName (name можно передать в конструктор). Класс Dog имеет метод bark (возвращает строку «Dog {dogname} is barking».
 Пример использования:
 
@@ -40,11 +7,12 @@ dog.getName () === 'Aban'; // true
 dog.bark () === 'Dog Aban is barking'; // true
 
 Задание необходимо сделать в двух вариантах: на ES5 и ES6.*/
+
 function Animal(){}
 function Dog(dogname){
 	this.name = dogname;
 	this.bark = function(){
-		return `Dog ${dogname} is barking`
+		return 'Dog '+dogname+' is barking'
 	}
 }
 
@@ -54,12 +22,22 @@ Animal.prototype.getName = function(){
 	return this.name
 }
 
-var dog = new Dog('Aban')
+var dog = new Dog ('Aban');
+console.dir(dog.bark())
 
-console.dir(dog)
+/**********************************/
 
-class Dog(dogname){
-	constructor: {
-		
+class AnimalES6 {
+	getName(){
+		return this.name
 	}
 }
+class DogES6 extends AnimalES6 {
+	constructor(dogname = ''){
+		super();
+		this.name = dogname;
+		this.bark = () => `Dog ${dogname} is barking`
+	}
+}
+var dogES6 = new DogES6('Bobik')
+console.dir(dogES6.bark())
